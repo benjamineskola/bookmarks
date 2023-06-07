68195f1c0b640591aa47b7f3edb109c9ca343ed9
Benjamin Eskola <ben@eskola.uk>
2023-06-07 21:56:07 +0100

    Add basic layout

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.GetHead)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Compress(5))
+	router.Use(middleware.URLFormat)
+
+	router.Get("/links", noopHandler)
+	router.Get("/links/page/{page}", noopHandler)
+	router.Get("/links/{id}", noopHandler)
+	router.Put("/links/{id}", noopHandler)
+	router.Delete("/links/{id}", noopHandler)
+	router.Post("/links", noopHandler)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Failed to start server: %s", err)
+	}
+}
+
+func noopHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("hello world\n"))
+}
