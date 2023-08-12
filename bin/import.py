b100f25fc1dc47cbdb2a31d5c295e5873b9ed940
Benjamin Eskola <ben@eskola.uk>
2023-08-12 18:32:43 +0100

    Add importers from CSV

---
+++ bin/import.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+import csv
+import datetime
+import sqlite3
+import sys
+
+
+def import_dropbox_csv(csvpath: str, dbpath: str, read: bool) -> None:
+    db = sqlite3.connect(dbpath)
+    cursor = db.cursor()
+    now = datetime.datetime.now()
+
+    with open(csvpath, newline="") as csvpath:
+        reader = csv.DictReader(csvpath, fieldnames=("URL", "Timestamp"))
+        for item in reader:
+            link = {
+                "url": item["URL"],
+                "date": datetime.datetime.strptime(
+                    item["Timestamp"].removesuffix("AM").removesuffix("PM"),
+                    "%B %d, %Y at %H:%M",
+                ),
+                "now": now,
+            }
+
+            if read:
+                cursor.execute(
+                    """UPDATE links SET updated_at=:now, read_at=:date
+                    WHERE url=:url AND read_at <> :date;""",
+                    link,
+                )
+                cursor.execute(
+                    """INSERT OR IGNORE INTO links
+                    (url, created_at, updated_at, saved_at, read_at)
+                    VALUES(:url, :now, :now, :now, :date);""",
+                    link,
+                )
+            else:
+                cursor.execute(
+                    """UPDATE links SET updated_at=:now, saved_at=:date
+                    WHERE url=:url AND saved_at <> :date;""",
+                    link,
+                )
+                cursor.execute(
+                    """INSERT OR IGNORE INTO links
+                        (url, created_at, updated_at, saved_at)
+                        VALUES(:url, :now, :now, :date);""",
+                    link,
+                )
+            db.commit()
+
+
+def import_instapaper_csv(csvpath: str, dbpath: str) -> None:
+    db = sqlite3.connect(dbpath)
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+
+    now = datetime.datetime.now()
+
+    with open(csvpath, newline="") as csvpath:
+        reader = csv.DictReader(csvpath)
+        for item in reader:
+            url = item["URL"]
+            title = item["Title"]
+            description = item["Selection"]
+            saved_at = datetime.datetime.fromtimestamp(int(item["Timestamp"]))
+            read_at = 0 if item["Folder"] == "Archive" else None
+            tags = {"instapaper"}
+
+            if item["Folder"] != "Archive":
+                tags.add(item["Folder"].lower())
+
+            entry = dict(
+                cursor.execute("SELECT * FROM links WHERE url=?", (url,)).fetchone()
+                or {}
+            )
+
+            if entry:
+                changed = False
+
+                for tag in entry["tags"].lower().strip("{}").split(","):
+                    if tag == "instapaper":
+                        continue
+                    tags.add(tag)
+                for tag in tags:
+                    if tag not in entry["tags"].lower().strip("{}").split(","):
+                        changed = True
+                        entry["tags"] = "{" + (",".join(tags)) + "}"
+                        break
+
+                if entry["title"] != title:
+                    entry["title"] = title
+                    changed = True
+                if entry["description"] != description:
+                    entry["description"] = description
+                    changed = True
+                if str(entry["read_at"]) != str(read_at):
+                    entry["read_at"] = read_at
+                    changed = True
+                if str(entry["saved_at"]) != str(saved_at):
+                    entry["saved_at"] = saved_at
+                    changed = True
+
+                if changed:
+                    entry["updated_at"] = now
+
+                    cursor.execute(
+                        """UPDATE links SET
+                        url=:url,
+                        created_at=:created_at,
+                        updated_at=:updated_at,
+                        saved_at=:saved_at,
+                        read_at=:read_at,
+                        title=:title,
+                        description=:description,
+                        tags=:tags
+                        WHERE url=:url;""",
+                        entry,
+                    )
+            else:
+                entry = {
+                    "url": url,
+                    "title": title,
+                    "description": description,
+                    "saved_at": saved_at,
+                    "read_at": read_at,
+                    "tags": "{" + (",".join(tags)) + "}",
+                    "created_at": now,
+                    "updated_at": now,
+                }
+                cursor.execute(
+                    """INSERT INTO links
+                    (url, created_at, updated_at, saved_at, read_at, title, description, tags)
+                    VALUES(:url, :created_at, :updated_at, :saved_at, :read_at, :title, :description, :tags);""",
+                    entry,
+                )
+
+            db.commit()
+
+
+if __name__ == "__main__":
+    csvpath, dbpath, *_ = sys.argv[1:]
+    import_instapaper_csv(csvpath, dbpath)
+    import_dropbox_csv(
+        "/Users/ben/Library/CloudStorage/Dropbox/IFTTT/Instapaper/Saved Items.txt",
+        dbpath,
+        False,
+    )
+    import_dropbox_csv(
+        "/Users/ben/Library/CloudStorage/Dropbox/IFTTT/Instapaper/Archived Items.txt",
+        dbpath,
+        True,
+    )
