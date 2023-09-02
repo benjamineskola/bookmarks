bfe2cec0934c3dbe7bea2409162d1dd448c743ef
Benjamin Eskola <ben@eskola.uk>
2023-09-02 09:44:32 +0100

    URL field should be unique

--- migrations/000001_create_links_table.up.sql
+++ migrations/000001_create_links_table.up.sql
@@ -3,12 +3,11 @@ CREATE TABLE IF NOT EXISTS "links" (
     created_at datetime,
     updated_at datetime,
     deleted_at datetime,
-    url text,
+    url text UNIQUE NOT NULL,
     title text,
     description text,
     saved_at datetime,
     read_at datetime
 );
 
-CREATE UNIQUE INDEX idx_links_url ON links (url);
 CREATE INDEX idx_links_deleted_at ON links (deleted_at);
